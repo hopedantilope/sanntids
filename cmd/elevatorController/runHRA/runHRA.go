4e8698ba5f8e0dfd6ca3f2e245728b91b262d126
haavard.hope <haavard.hope@maritimerobotics.com>
2025-03-04 17:02:32 +0100

    here

--- cmd/elevatorController/runHRA/runHRA.go
+++ cmd/elevatorController/runHRA/runHRA.go
@@ -7,6 +7,7 @@ import "encoding/json"
 import "runtime"
 import "sanntids/cmd/localElevator/elevator"
 import "sanntids/cmd/localElevator/config"
+import "os"
 
 // Struct members must be public in order to be accessible by json.Marshal/.Unmarshal
 // This means they must start with a capital letter, so we need to use field renaming struct tags to make them camelCase
@@ -58,25 +59,25 @@ func runHRA(hallRequests [config.N_FLOORS][2]bool, elevators map[string]elevator
     jsonBytes, err := json.Marshal(input)
     if err != nil {
         fmt.Println("json.Marshal error: ", err)
-        return
+        return nil
     }
     path,_:=os.Getwd();
     ret, err := exec.Command(path + "elevatorController/cost_fns/hall_request_assigner/"+hraExecutable, "-i", string(jsonBytes)).CombinedOutput()
     if err != nil {
         fmt.Println("exec.Command error: ", err)
         fmt.Println(string(ret))
-        return
+        return nil
     }
     
     output := make(map[string][config.N_FLOORS][2]bool)
     err = json.Unmarshal(ret, &output)
     if err != nil {
         fmt.Println("json.Unmarshal error: ", err)
-        return
+        return nil
     }
         
     fmt.Printf("output: \n")
-    for k, v := range *output {
+    for k, v := range output {
         fmt.Printf("%6v :  %+v\n", k, v)
     }
 
