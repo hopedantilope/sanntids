873156f5cf7e6d3939c7e9c2e710211de777e5d7
haavard.hope <haavard.hope@maritimerobotics.com>
2025-03-25 19:32:51 +0100

    lol

--- cmd/networkOrders/networkOrders.go
+++ cmd/networkOrders/networkOrders.go
@@ -75,11 +75,10 @@ func NetworkOrderManager(
 						if order.Floor == newOrder.Floor && order.Dir == newOrder.Dir {
 							// Handle messages from the master:
 							if util.IsMaster(ipMap, incomingData.ElevatorID) {
-								if order.Status == structs.New && newOrder.Status == structs.Completed {
-									continue
+								if !(order.Status == structs.New && newOrder.Status == structs.Completed) {
+									hallOrders[i].Status = newOrder.Status
+									hallOrders[i].DelegatedID = newOrder.DelegatedID
 								}
-								hallOrders[i].Status = newOrder.Status
-								hallOrders[i].DelegatedID = newOrder.DelegatedID
 							}
 							// The master should only accept certain orders:
 							if util.IsMaster(ipMap, localElevatorID) {
@@ -90,6 +89,7 @@ func NetworkOrderManager(
 										hallOrders[i].DelegatedID = newOrder.DelegatedID
 									}
 								case structs.Assigned:
+
 								case structs.Confirmed:
 								case structs.Completed:
 									if order.Status != structs.New && order.DelegatedID == incomingData.ElevatorID{
